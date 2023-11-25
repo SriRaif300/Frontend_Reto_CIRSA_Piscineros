c5a1d8c66c70c9f7854bd386e4dc9de5066b5a36
Camilo Andres Garatejo Moreno <79905957+SriRaif300@users.noreply.github.com>
2023-11-25 09:29:23 +0100

    Add project files.

---
+++ Frontend_Reto_CIRSA_Piscineros/Program.cs
@@ -0,0 +1,11 @@
+using Frontend_Reto_CIRSA_Piscineros;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
